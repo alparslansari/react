24c9216c0567a7c29364b63e6b07ce7a1b6d18fc
asari@udel.edu <asari@udel.edu>
2021-12-17 09:30:27 -0500

    spinner example

--- react-comp/src/components/Speaker.js
+++ react-comp/src/components/Speaker.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 function Session({ title, room }) {
     return (
         <span className="session w-100">
@@ -28,19 +30,24 @@ function SpeakerImage({ id, first, last}) {
 }
 
 function SpeakerFavorite({ favorite, onFavoriteToggle }) {
-
+    const [inTransition, setInTransition] = useState(false); // default to false
     function doneCallback() {
+        setInTransition(false);
         console.log(`In SpeakerFavorite:doneCallback ${new Date().getMilliseconds()}`);
     }
 
     return (
         <div className="action padB1">
             <span onClick={function() {
+                setInTransition(true);
                 return onFavoriteToggle(doneCallback);
             }} >
                 <i className={
                     favorite === true ? "fa fa-star orange": "fa fa-star-o orange"
                 }/>{" "} Favorite{" "}
+                {inTransition ? (
+                    <span className='fas fa-circle-notch fa-spin'></span>
+                ): null}
             </span>
         </div>
     )
