fab042328f5aff4fd7176478e87934a80970d5e6
asari@udel.edu <asari@udel.edu>
2021-12-16 22:44:56 -0500

    use effects

--- react-comp/src/components/Speakers.js
+++ react-comp/src/components/Speakers.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import SpeakersList from "./SpeakersList";
 import SpeakersToolbar from "./SpeakersToolbar";
 
-function Speakers({ data, theme, setTheme}){
+function Speakers({ theme, setTheme}){
     const [showSessions, setShowSessions] = useState(true); // set initial value
 
     return (
         <>
             <SpeakersToolbar theme={theme} setTheme={setTheme} 
                 showSessions={showSessions} setShowSessions={setShowSessions}/>
-            <SpeakersList data={data} showSessions={showSessions}/>
+            <SpeakersList showSessions={showSessions}/>
         </>
     )
 
--- react-comp/src/components/SpeakersList.js
+++ react-comp/src/components/SpeakersList.js
@@ -1,12 +1,47 @@
 import Speaker from './Speaker'
+import { data } from '../../SpeakerData';
+import { useState, useEffect } from "react";
 
-function SpeakersList({ data, showSessions }) {
+function SpeakersList({ showSessions }) {
+    const [speakersData, setSpeakersData] = useState([]);
+
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    useEffect(() =>{
+        async function delayFunc(){
+            await delay(2000); // code waits 2 seconds
+            setSpeakersData(data);
+            console.log("useEffect");
+        }
+        delayFunc();
+    }); // use effect runs for every rerender like click events etc.
+    // }, []); second array should be added to run this function once
+    // in only initialization
+
+    function onFavoriteToggle(id) {
+        const speakersRecPrevious = speakersData.find(function (rec) {
+            return rec.id === id;
+        });
+        const speakerRecUpdated = {
+            ...speakersRecPrevious,
+            favorite: !speakersRecPrevious.favorite
+        };
+        const speakersDataNew = speakersData.map(function (rec){
+            return rec.id === id ? speakerRecUpdated : rec;
+        });
+
+        setSpeakersData(speakersDataNew);
+    }
+
     return(
         <div className="container speakers-list">
             <div className="row">
-                {data.map(function (speaker){
+                {speakersData.map(function (speaker){
                     return (
-                        <Speaker key={speaker.id} speaker={speaker} showSessions={showSessions} />
+                        <Speaker key={speaker.id} speaker={speaker} showSessions={showSessions} 
+                        onFavoriteToggle={()=>{
+                            onFavoriteToggle(speaker.id);
+                        }}/>
                     );
                 })}
             </div>
